120bf4f3c332eb434f165f3841d2e31b90552846
bin862324915 <862324915@qq.com>
2023-08-27 18:47:34 +0800

    Site updated: 2023-08-27 18:47:32

--- js/zidingyi/music.js
+++ js/zidingyi/music.js
@@ -3,9 +3,16 @@ const ap = new APlayer({
     fixed: true,
     autoplay: true, //自动播放
     audio: [{
-        name: '你好吗',
-        artist: '我很好',
-        url: 'http://music.163.com/song/media/outer/url?id=139774.mp3',
-        cover: '/img/avatar.jpg',
-    }, ]
+            name: '你好吗',
+            artist: '我很好',
+            url: 'http://music.163.com/song/media/outer/url?id=139774.mp3',
+            cover: 'https://p1.music.126.net/JvCzECg-bM3ZvePvKwBAEw==/109951165390158816.jpg',
+        },
+        {
+            name: '去有风的地方',
+            artist: '遇见你',
+            url: 'https://aod.cos.tx.xmcdn.com/storages/5ad5-audiofreehighqps/B1/49/GKwRINsH-5EFABwOWQII5-0b.m4a',
+            cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000001jmGnd4KUxDv.jpg',
+        },
+    ]
 });
