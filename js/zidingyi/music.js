1a77663b4f7cd10747b21259b651c75b305b838d
bin862324915 <862324915@qq.com>
2023-08-28 00:29:20 +0800

    Site updated: 2023-08-28 00:29:20

--- js/zidingyi/music.js
+++ js/zidingyi/music.js
@@ -14,5 +14,11 @@ const ap = new APlayer({
             url: 'https://aod.cos.tx.xmcdn.com/storages/5ad5-audiofreehighqps/B1/49/GKwRINsH-5EFABwOWQII5-0b.m4a',
             cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000001jmGnd4KUxDv.jpg',
         },
+        {
+            name: '愿世间美好',
+            artist: '与你环环相扣',
+            url: 'http://ws.stream.qqmusic.qq.com/C4000010EnTc3omLSF.m4a?guid=436322718&vkey=3A76DACA66FFA73B274F1E766A7950248E2E66C5706A56E0C80FA9384F18C256F9E747C0C18CF4C917744FB9D6C868B05B37F51F70DB850D&uin=&fromtag=120032',
+            cover: 'https://y.qq.com/music/photo_new/T002R300x300M000003BZfLE0ba4fQ_1.jpg',
+        },
     ]
 });
