e5181c80ea60adad6b915d090bcecca08c03ff78
bin862324915 <862324915@qq.com>
2023-08-29 21:33:05 +0800

    Site updated: 2023-08-29 21:33:01

--- js/zidingyi/dist/music.js
+++ js/zidingyi/dist/music.js
@@ -3,15 +3,15 @@ const ap = new APlayer({
     fixed: true,
     autoplay: true, //自动播放
     audio: [{
-            name: '你好吗',
-            artist: '我很好',
-            url: 'http://ws.stream.qqmusic.qq.com/C4000010EnTc3omLSF.m4a?guid=436322718&vkey=3A76DACA66FFA73B274F1E766A7950248E2E66C5706A56E0C80FA9384F18C256F9E747C0C18CF4C917744FB9D6C868B05B37F51F70DB850D&uin=&fromtag=120032',
+            name: '未来可期',
+            artist: '人生值得',
+            url: 'https://aod.cos.tx.xmcdn.com/storages/5ad5-audiofreehighqps/B1/49/GKwRINsH-5EFABwOWQII5-0b.m4a',
             cover: 'https://y.qq.com/music/photo_new/T002R300x300M000003BZfLE0ba4fQ_1.jpg',
         },
         {
             name: '愿世间美好',
             artist: '与你环环相扣',
-            url: 'https://aod.cos.tx.xmcdn.com/storages/5ad5-audiofreehighqps/B1/49/GKwRINsH-5EFABwOWQII5-0b.m4a',
+            url: 'http://ws.stream.qqmusic.qq.com/C4000010EnTc3omLSF.m4a?guid=436322718&vkey=3A76DACA66FFA73B274F1E766A7950248E2E66C5706A56E0C80FA9384F18C256F9E747C0C18CF4C917744FB9D6C868B05B37F51F70DB850D&uin=&fromtag=120032',
             cover: 'https://p1.music.126.net/JvCzECg-bM3ZvePvKwBAEw==/109951165390158816.jpg',
         },
         {
