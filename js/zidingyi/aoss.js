1417d5937b5ee30e53f5dd04c67ff929f4e586e4
bin862324915 <862324915@qq.com>
2023-09-08 00:21:23 +0800

    Site updated: 2023-09-08 00:21:22

--- js/zidingyi/aoss.js
+++
@@ -1,7 +0,0 @@
-
-
-"use strict";
-
-document.addEventListener('DOMContentLoaded', function() { AOS.init({ offset: 200, duration: 600, easing: 'ease-in-sine', delay: 100, }); });
-
-module.exports = { init: function() { document.addEventListener('DOMContentLoaded', function() { AOS.init({ offset: 200, duration: 600, easing: 'ease-in-sine', delay: 100, }); }); } };
