9072253e5435770e12694c7c34ba12146c9e8576
bin862324915 <862324915@qq.com>
2023-10-12 23:46:03 +0800

    Site updated: 2023-10-12 23:45:53

--- js/zidingyi/zdy.js
+++ js/zidingyi/zdy.js
@@ -26,3 +26,24 @@ document.addEventListener('visibilitychange', function() {
 		}, 2000);
 	}
 });
+
+// 背景图
+var backgroundImageUrls = [
+  "url('https://z1.ax1x.com/2023/10/11/pPzRbo4.jpg')",
+  "url('https://z1.ax1x.com/2023/09/12/pP216Vx.jpg')",
+  "url('https://z1.ax1x.com/2023/10/12/piSvJts.png')",
+  "url('https://z1.ax1x.com/2023/10/12/piSv10g.jpg')",
+  "url('https://z1.ax1x.com/2023/10/12/piSv37Q.jpg')",
+  "url('https://z1.ax1x.com/2023/10/12/piSvlnS.jpg')",
+  "url('https://z1.ax1x.com/2023/10/12/piSvMX8.jpg')",
+  "url('https://z1.ax1x.com/2023/10/12/piSvGkj.png')",
+  "url('https://z1.ax1x.com/2023/10/12/piSvu1P.jpg')",
+  "url('https://z1.ax1x.com/2023/10/12/piSvefI.jpg')",
+  "url('https://z1.ax1x.com/2023/10/12/piSvnpt.jpg')",
+  "url('https://z1.ax1x.com/2023/10/12/piSvK6f.jpg')",
+  "url('https://z1.ax1x.com/2023/09/12/pP21sq1.jpg')"
+];
+
+var randomIndex = Math.floor(Math.random() * backgroundImageUrls.length);
+var bodyElement = document.querySelector("body");
+bodyElement.style.backgroundImage = backgroundImageUrls[randomIndex];
